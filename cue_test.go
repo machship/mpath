71b8b2ca26b0cd9b9b7c1d2589732ed2ddc20eab
Mike McKay <mmckay@machship.com>
2023-11-24 10:17:02 +1100

    More tests

--- cue_test.go
+++ cue_test.go
@@ -3,42 +3,7 @@ package mpath
 import (
 	"encoding/json"
 	"testing"
-
-	"github.com/atotto/clipboard"
 )
-
-func Test_CueFromString(t *testing.T) {
-	cueString := `
-	step1: {
-		num: int
-		_dependencies: [] 
-		result: [{
-			name: string
-			age: int
-		}]
-	}  
-	
-	step2: {
-		_dependencies: ["step1"]
-		result: [...{
-			name: string
-			age: int
-		}]
-	}
-	`
-
-	mpathQuery := `$.step1.result[@.age.Equal($.step1.num)].First()`
-	tc, _, err := CueValidate(mpathQuery, cueString, "step2")
-	if err != nil {
-		t.Error(err)
-	}
-
-	jstr, err := json.MarshalIndent(tc, "", "\t")
-	if err != nil {
-		t.Error(err)
-	}
-	clipboard.WriteAll(string(jstr))
-}
 
 func Test_CueStringTableTests(t *testing.T) {
 	type tableTest struct {
@@ -58,6 +23,12 @@ func Test_CueStringTableTests(t *testing.T) {
 			name: "check that array with ... can be assessed",
 			mq:   `$.step2.result[@.age.Equal($.step2.num)].First()`,
 			cp:   "step3",
+		},
+		{
+			name:         "try to access step that is not available",
+			mq:           `$.step2.result[@.age.Equal($.step2.num)].First()`,
+			cp:           "step2",
+			expectErrors: true,
 		},
 	}
 
