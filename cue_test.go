bd9fd6ced29eaafb518c71631507dd9c06b05489
Michael McKay <michaelmckay@machship.com>
2024-01-31 11:05:03 +1100

    Added some benchmarks

--- cue_test.go
+++ cue_test.go
@@ -2,20 +2,351 @@ package mpath
 
 import (
 	"encoding/json"
+	"sort"
 	"testing"
 
 	"github.com/atotto/clipboard"
 )
 
-func Test_CueStringTableTests(t *testing.T) {
-	type tableTest struct {
-		name         string
-		mq           string
-		expectErrors bool
-		cp           string
-	}
+func Benchmark_CueStringTableTests(b *testing.B) {
+	sort.Slice(cueTableTests, func(i, j int) bool {
+		return len(cueTableTests[i].name) > len(cueTableTests[j].name)
+	})
 
-	tests := []tableTest{
+	for _, test := range cueTableTests {
+		b.Run(test.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				b.ReportAllocs()
+				tc, err := CueValidate(test.mq, cueStringForTests, test.cp)
+				if err != nil && !test.expectErrors {
+					b.Errorf("test '%s'; got unexpected returned error: %v", test.name, err)
+				}
+				if tc != nil && tc.HasErrors() != test.expectErrors {
+					b.Errorf("test '%s'; expected %t got %t for HasErrors(); err was '%v'", test.name, test.expectErrors, tc.HasErrors(), tc.GetErrors())
+				}
+				b.SetBytes(int64(len(test.mq + cueStringForTests + test.cp)))
+			}
+		})
+	}
+}
+
+func Benchmark_BigCueFile(b *testing.B) {
+
+	cueFile := `"input": {
+    num: int,
+    _dependencies: []
+}
+"3dedbf75-1c91-4ec5-8018-99b1efe47462": {
+    result: int,
+    _dependencies: []
+}
+"52a015ef-6e51-407d-82e2-72fb218ae65b": {
+    result: {
+        #company: {
+            id: number
+            address?: {
+                name?: string
+                contact?: string
+                phone?: string
+                email?: string
+                addressLine1?: string
+                addressLine2?: string
+                addressLinesFriendly?: string
+                hasAddressLine2: bool
+            }
+            name: string
+            accountCode: string
+            isEnterprise: bool
+        }
+        
+        consignments: [...{
+                id: number
+                consignmentNumber: string
+                carrierConsignmentId: string
+                dateCreated: string
+                insertedBy: number
+                service: {
+                    name: string
+                    abbreviation: string
+                    serialisedSettings?: {...}
+                }
+                despatchDateTime: string
+                fromAddress: {
+                    location: {
+                        id: number
+                        postcode: string
+                        paddedPostcode: string
+                        state: {
+                            id: number
+                            name: string
+                            code: string
+                        }
+                        subLocality?: string
+                        suburb: string
+                        description: string
+                        timeZone: string
+                        country: {
+                            id: number
+                            name: string
+                            code2: string
+                            code3: string
+                            numeric: string
+                            isInternational: bool
+                        }
+                    }
+                    name: string
+                    contact?: string
+                    phone?: string
+                    email?: string
+                    addressLine1: string
+                    addressLine2?: string
+                    carrierZone?: {
+                        id: number
+                        name: string
+                        abbreviation: string
+                    }
+                }
+                fromLocationCarrierZoneSettings?: {...}
+                toAddress: {
+                    location: {
+                        id: number
+                        postcode: string
+                        paddedPostcode: string
+                        state: {
+                            id: number
+                            name: string
+                            code: string
+                        }
+                        subLocality?: string
+                        suburb: string
+                        description: string
+                        timeZone: string
+                        country: {
+                            id: number
+                            name: string
+                            code2: string
+                            code3: string
+                            numeric: string
+                            isInternational: bool
+                        }
+                    }
+                    name: string
+                    contact?: string
+                    phone?: string
+                    email?: string
+                    addressLine1: string
+                    addressLine2?: string
+                    carrierZone?: {
+                        id: number
+                        name: string
+                        abbreviation: string
+                    }
+                }
+                toLocationCarrierZoneSettings?: {...}
+                specialInstructions?: string
+                customerReference?: string
+                customerReference2?: string
+                totalWeight: number
+                totalCubic: number
+                totalVolume: number
+                validFromCompanyLocation: bool
+                validToCompanyLocation: bool
+                fromCompanyLocationId?: number
+                toCompanyLocationId?: number
+                fromCompanyLocationAbbreviation?: string
+                toCompanyLocationAbbreviation?: string
+                receiverCode?: string
+                isReceiverPays: bool
+                isDgConsignment: bool
+                dgsDeclaration: bool
+                dgsDeclarationDateTimeUtc?: string
+                dgsDeclarationUserId?: number
+                emergencyContactNumber?: string
+                surcharges?: [...{
+                    name: string      
+                    abbreviation: string      
+                    quantity: number      
+                }]
+                etaLocal: string
+                primaryReferenceType: number
+                primaryReference: string
+                carrierReference?: string
+                totalItemCount: number
+                items: [...{
+                        id: number
+                        carrierItemType?: string
+                        itemType: string
+                        name: string
+                        sku?: string
+                        height: number
+                        weight: number
+                        length: number
+                        width: number
+                        quantity: number
+                        palletSpaces?: number
+                        volume: number
+                        isDgItem: bool
+                        integrationItemDgItems?: [...{
+                                id: number
+                                companyDgItemId?: number
+                                unNumber: number
+                                packingGroup: number
+                                packingGroupStringValue: string
+                                containerType: number
+                                containerTypeStringValue: string
+                                aggregateQuantity: number
+                                isAggregateQuantityWeight: bool
+                                numberOfContainers: number
+                                dgClassType: number
+                                dgClassTypeStringValue: string
+                                dgClassTypeStringDescription: string
+                                subDgClassTypes?: [...number]
+                                subDgClassTypesSeparator?: string
+                                subDgClassTypesStringValue?: string
+                                hasSubRisk: bool
+                                properShippingNameValue: string
+                                properShippingName: string
+                                technicalOrChemicalGroupNames?: string
+                                hasTechnicalName: bool
+                                isMarinePollutant: bool
+                                isTemperatureControlled: bool
+                                isEmptyDgContainer: bool
+                                quantityPerContainer: number
+                                totalAggregateQuantity: number
+                                totalNumberOfContainers: number
+                                dgClassDisplay: string
+                            }
+                        ]
+                        integrationItemReferences?: [...{
+                                id: number
+                                carrierItemReference: string
+                                itemNumber: number
+                                carrierReference?: string
+                            }
+                        ]
+                        integrationItemContents?: [...{
+                                id: number
+                                description: string
+                                reference1?: string
+                                reference2?: string
+                                reference3?: string
+                                quantity: number
+                                dollarValue?: number
+                                ciMarksAndNumbers?: string
+                                harmonizedCode?: string
+                                partNumber?: string
+                                purpose?: string
+                                countryOfManufactureId?: string
+                                countryOfManufacture?: {
+                                    id: number
+                                    name: string
+                                    code2: string
+                                    code3: string
+                                    numeric: string
+                                }
+                            }
+                        ]
+                    }
+                ]
+                internationalFromCity?: string
+                internationalFromPostcode?: string
+                internationalFromProvince?: string
+                fromCountry?: string
+                internationalToCity?: string
+                internationalToPostcode?: string
+                internationalToProvince?: string
+                toCountry?: string
+                isInternational: bool
+            }
+        ]
+        pickup: {
+            pickupDateTime: string
+            palletSpaces?: number
+            pickupBooked: bool
+            pickupClosingTime?: string
+            pickupSpecialInstructions?: string
+            pickupRequired: bool
+            permanentPickup: bool
+            totalWeightKgs: number
+        }
+        consigningCompany: #company
+        accountHolderCompany: #company
+        carrier: {
+            id: number
+            name: string
+            code: string
+            serialisedSettings?: {...}
+        }
+        account: {
+            name: string
+            accountCode?: string
+            abbreviation: string
+            serialisedSettings?: {...}
+        }
+        bookingType?: string
+    },
+    _dependencies: ["3dedbf75-1c91-4ec5-8018-99b1efe47462"]
+}
+"bd33058f-d866-4800-aa97-098c0137e8c0": {
+    result: string
+    _dependencies: ["52a015ef-6e51-407d-82e2-72fb218ae65b"]
+}`
+
+	testName := "Big Cue"
+
+	b.Run(testName, func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			b.ReportAllocs()
+			tc, err := CueValidate(`$.52a015ef-6e51-407d-82e2-72fb218ae65b.result.consignments`, cueFile, `bd33058f-d866-4800-aa97-098c0137e8c0`)
+			if err != nil {
+				b.Errorf("test '%s'; got unexpected returned error: %v", testName, err)
+			}
+			if tc != nil && tc.HasErrors() != false {
+				b.Errorf("test '%s'; expected %t got %t for HasErrors(); err was '%v'", testName, false, tc.HasErrors(), tc.GetErrors())
+			}
+			b.SetBytes(int64(len(`$.52a015ef-6e51-407d-82e2-72fb218ae65b.result.consignments` + cueFile + `bd33058f-d866-4800-aa97-098c0137e8c0`)))
+		}
+	})
+}
+
+func Test_CueStringTableTests(t *testing.T) {
+
+	var onlyRunTest string
+	var copyAndLog bool
+
+	// onlyRunTest = "test get error for incomplete"
+	// copyAndLog = true
+
+	for _, test := range cueTableTests {
+		if onlyRunTest != "" && test.name != onlyRunTest {
+			continue
+		}
+
+		tc, err := CueValidate(test.mq, cueStringForTests, test.cp)
+		if err != nil && !test.expectErrors {
+			t.Errorf("test '%s'; got unexpected returned error: %v", test.name, err)
+		}
+		if tc != nil && tc.HasErrors() != test.expectErrors {
+			t.Errorf("test '%s'; expected %t got %t for HasErrors(); err was '%v'", test.name, test.expectErrors, tc.HasErrors(), tc.GetErrors())
+		}
+
+		if copyAndLog {
+			tcb, _ := json.MarshalIndent(tc, "", "  ")
+			clipboard.WriteAll(string(tcb))
+			t.Log(string(tcb))
+		}
+	}
+}
+
+type tableTest struct {
+	name         string
+	mq           string
+	expectErrors bool
+	cp           string
+}
+
+var (
+	cueTableTests = []tableTest{
 		{
 			name: "check that array without ... can be assessed",
 			mq:   `$.step1.result[@.age.Equal($.step1.num)].First()`,
@@ -87,33 +418,7 @@ func Test_CueStringTableTests(t *testing.T) {
 			expectErrors: true,
 		},
 	}
-
-	var onlyRunTest string
-	var copyAndLog bool
-
-	// onlyRunTest = "test get error for incomplete"
-	// copyAndLog = true
-
-	for _, test := range tests {
-		if onlyRunTest != "" && test.name != onlyRunTest {
-			continue
-		}
-
-		tc, err := CueValidate(test.mq, cueStringForTests, test.cp)
-		if err != nil && !test.expectErrors {
-			t.Errorf("test '%s'; got unexpected returned error: %v", test.name, err)
-		}
-		if tc != nil && tc.HasErrors() != test.expectErrors {
-			t.Errorf("test '%s'; expected %t got %t for HasErrors(); err was '%v'", test.name, test.expectErrors, tc.HasErrors(), tc.GetErrors())
-		}
-
-		if copyAndLog {
-			tcb, _ := json.MarshalIndent(tc, "", "  ")
-			clipboard.WriteAll(string(tcb))
-			t.Log(string(tcb))
-		}
-	}
-}
+)
 
 const (
 	cueStringForTests = `
